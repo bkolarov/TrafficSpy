00e39dd49f975910a300f764ba1d648abc01daf1
crash-id <kbogomil@gmail.com>
2015-03-24 02:33:19 +0200

    Add marker on touch added.

--- AndroidApp/TrafficSpy/src/org/elsys/internetprogramming/trafficspy/Map.java
+++ AndroidApp/TrafficSpy/src/org/elsys/internetprogramming/trafficspy/Map.java
@@ -9,10 +9,12 @@ import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMap.OnCameraChangeListener;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class Map extends BroadcastReceiver implements OnCameraChangeListener {
+public class Map extends BroadcastReceiver implements OnCameraChangeListener, OnMapClickListener {
 	public static final String BROADCAST_RECEIVER_BANE = "LocationUpdates";
 	private final String TAG = "MAP";
 	
@@ -26,6 +28,7 @@ public class Map extends BroadcastReceiver implements OnCameraChangeListener {
 		this.map.setMyLocationEnabled(true);
 		this.map.setOnCameraChangeListener(this);
 		this.map.setTrafficEnabled(true);
+		this.map.setOnMapClickListener(this);
 	}
 
 	@Override
@@ -45,6 +48,12 @@ public class Map extends BroadcastReceiver implements OnCameraChangeListener {
 	private void moveCamera(final LatLng latLng) {
 		final CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, this.MAX_MAP_ZOOM);
 		this.map.animateCamera(cameraUpdate);
+	}
+
+	@Override
+	public void onMapClick(LatLng point) {
+		this.map.addMarker(new MarkerOptions().position(point));
+		
 	}
 	
 }
