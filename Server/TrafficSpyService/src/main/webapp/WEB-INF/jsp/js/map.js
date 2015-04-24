fa05d8996a03118ae316349da7048ee5abf3eaf8
Bogomil <Crash-ID@users.noreply.github.com>
2015-04-24 19:42:41 +0300

    The script now is able to add markers.

--- Server/TrafficSpyService/src/main/webapp/WEB-INF/jsp/js/map.js
+++ Server/TrafficSpyService/src/main/webapp/WEB-INF/jsp/js/map.js
@@ -1,18 +1,48 @@
-jQuery(window).ready(function(){  
-	        initiate_geolocation();  
-	    });  
-	    function initiate_geolocation() {  
-	        navigator.geolocation.getCurrentPosition(handle_geolocation_query);  
-	    }  
-	    function handle_geolocation_query(position){
-	    var lng = position.coords.longitude;
-	    var lat = position.coords.latitude;
+var map;
+			
+function initiate_geolocation() {  
+    navigator.geolocation.getCurrentPosition(handle_geolocation_query);  
+}  
 
-	        var mapOptions = {
-	          center: new google.maps.LatLng(lat, lng),
-	          zoom: 16
-	        };
-	        var map = new google.maps.Map(document.getElementById("map"),
-	            mapOptions);
-	      }
-	      google.maps.event.addDomListener(window, 'load', initialize);  
+function handle_geolocation_query(position) {
+	var lng = position.coords.longitude;
+	var lat = position.coords.latitude;
+
+	var mapOptions = {
+	  center: new google.maps.LatLng(lat, lng),
+	  zoom: 13
+	};
+	map = new google.maps.Map(document.getElementById("map"), mapOptions);
+	
+	getMarkers();
+	
+	addMarker(42.672798698720065, 23.303494937717915);
+}
+
+function getMarkers() {
+	$.ajax({
+		 url:"http://ec2-52-28-51-57.eu-central-1.compute.amazonaws.com:8181/markers",
+		 success:function(json) {
+			json.forEach(function(obj) { 
+			addMarker(obj.latitude, obj.longitude); 
+		});
+		 },
+		 error:function(){
+			 alert("Error");
+		 }      
+	});
+
+
+}
+
+function addMarker(latitude, longitude) {
+	myLatlng = new google.maps.LatLng(latitude, longitude);
+	
+	marker = new google.maps.Marker({
+		position: myLatlng,
+		title: 'Hello World!'
+  	});
+	marker.setMap(map);
+}
+
+google.maps.event.addDomListener(window, 'load', initiate_geolocation);
